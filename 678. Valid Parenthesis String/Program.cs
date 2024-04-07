efef427eb9904bbfffbc3382e213d50e09dc4549
Chris Bouwhuis <chrisb190203@gmail.com>
2024-04-07 12:28:50 +0200

    Add project files.

---
+++ 678. Valid Parenthesis String/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLib;
+namespace _678._Valid_Parenthesis_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "((((()(()()()*()(((((*)()*(**(())))))(())()())(((())())())))))))(((((())*)))()))(()((*()*(*)))(*)()";
+            var sol = new Solution();
+            bool result = sol.CheckValidString(s);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ClassLib/Class1.cs
@@ -0,0 +1,48 @@
+﻿namespace ClassLib
+{
+    public class Solution
+    {
+        public bool CheckValidString(string s)
+        {
+            Stack<int> left = new Stack<int>();
+            Stack<int> star = new Stack<int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    left.Push(i);
+                }
+                else if (s[i] == '*')
+                {
+                    star.Push(i);
+                }
+                else
+                {
+                    if (left.Count > 0)
+                    {
+                        left.Pop();
+                    }
+                    else if (star.Count > 0)
+                    {
+                        star.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            while (left.Count > 0 && star.Count > 0)
+            {
+                if (left.Pop() > star.Pop())
+                {
+                    return false;
+                }
+            }
+
+            return left.Count == 0;
+        }
+    }
+}
